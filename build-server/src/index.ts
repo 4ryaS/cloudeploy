b20ca2901cc6f79aa31cd1318178170ac5722349
Dev Harwani <devharwani.work@gmail.com>
2025-02-12 01:10:55 +0530

    check valid git url

--- build-server/src/index.ts
+++ build-server/src/index.ts
@@ -5,6 +5,11 @@ import { nanoid } from "nanoid";
 const app = express();
 app.use(express.json());
 
+const gitUrlRegex = /^(?:git@[\w.-]+:[\w./-]+\.git|https?:\/\/[\w.-]+\/[\w./-]+\.git)$/;
+
+function isValidGitCloneUrl(url: string) {
+    return gitUrlRegex.test(url);
+}
 
 app.post("/deploy", async (req, res) => {
     const id = nanoid().toLowerCase();
@@ -16,6 +21,14 @@ app.post("/deploy", async (req, res) => {
         })
         return;
     };
+
+    if(!isValidGitCloneUrl(repoUrl)){
+        res.json({
+            message: "the provided url is invalid"
+        })
+        return;
+    }
+    
 
     try {
         await run(repoUrl, id);
